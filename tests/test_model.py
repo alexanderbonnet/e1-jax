859ad63fd3cdcd8e9b7e2ba3d608eb097cca4f89
alexanderbonnet <alex.bonnet.us@gmail.com>
2025-12-05 23:19:12 +0100

    expose less stuff

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,16 +1,16 @@
 import jax
 import jax.numpy as jnp
 
-from e1jax import e1, tokenizer
+from e1jax import _model, _tokenizer
 
 
 def test_padding_masking() -> None:
     pad_len = 17
     seq = "ACDEFGHIKLMNPQRSTVWY"
-    tokenized = tokenizer.tokenize(seq)
-    padded = tokenizer.pad_and_mask(tokenized, pad_length=pad_len)
+    tokenized = _tokenizer.tokenize(seq)
+    padded = _tokenizer.pad_and_mask(tokenized, pad_length=pad_len)
 
-    model = e1.E1(dim=128, num_layers=6, num_heads=4, ff_dim=256, key=jax.random.PRNGKey(0))
+    model = _model.E1(dim=128, num_layers=6, num_heads=4, ff_dim=256, key=jax.random.PRNGKey(0))
     logits, embeddings = model(**tokenized._asdict())
     logits_pad, embeddings_pad = model(**padded._asdict())
 
